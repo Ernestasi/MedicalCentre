e45cef6aaa004a2f7315ce3161d1e8b19a72e6de
domantasalenskas <domantas.alenskas@gmail.com>
2017-03-29 20:33:09 +0300

    Exit veikia

--- src/Login.java
+++ src/Login.java
@@ -4,6 +4,8 @@
 //
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
@@ -89,7 +91,12 @@ class Login {
 
         JButton loginExit = new JButton("Exit");
         window.add(loginExit);
-
+        loginExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
         window.setAlwaysOnTop(true);
         window.pack();
         window.setVisible(true);
